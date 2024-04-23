e33df0eb48aacef3952692f101ce20f5862ab2c4
Martín Perez <100448340+TinchoGithub@users.noreply.github.com>
2024-04-23 10:59:21 -0300

    Actualizo README y agrego sweetalert2 al login y registro a travez de una function en el evento onClick

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,7 +1,34 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+
+function register(){
+    Swal.fire({
+        title: "¡Felicidades! Te has registrado con éxito",
+        width: 600,
+        padding: "3em",
+        color: "#0d6efd",
+        background: "#fff url()",
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("../assets/img/papelitos.gif")
+          left top
+          
+        `
+    }).then((result) => {
+           if (result.isConfirmed) {
+            window.location.href = "../index.html";
+        }
+    });
+}
+
+function login(){
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "¡Bienvido!",
+        showConfirmButton: false,
+        timer: 1500
+      });
+      setTimeout(() => {
+        window.location.href = "./pages/home.html";
+    }, 1000);
+}
